b16e789f07b3ca6bebd6dfb80fe41ac8863f5a67
Sophie Phoon <sophie.j.phoon@gmail.com>
2020-09-12 12:00:05 +0800

    css added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import TodoItem from './components/TodoItem';
-import TodoForm from './components/TodoForm';
-
+import TodoItem from './components/TodoItem/TodoItem';
+import TodoForm from './components/TodoForm/TodoForm';
+import "./App.css";
 
 function App() {
     // variables
@@ -19,12 +19,14 @@ function App() {
 
     // template
     return (
-        <div>
-            <h1>To do list</h1>
-            {todos.map((todo, index) => (
-                <TodoItem todo={todo} key={index} />
-            ))}
-            <TodoForm addTodo={addTodo} />
+        <div className="app">
+            <div className="todo-list">
+                <h1>To do list</h1>
+                {todos.map((todo, index) => (
+                    <TodoItem todo={todo} key={index} />
+                ))}
+                <TodoForm addTodo={addTodo} />
+            </div>
         </div>
     );
 }
