6be76d4434060c647f2da2d443bb8bb2a3e350f6
Sophie Phoon <sophie.j.phoon@gmail.com>
2020-09-12 11:16:10 +0800

    Created todoitem component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoItem from './components/TodoItem';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Change <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>My To-Do List</h1>
+            <TodoItem todo="Finish plus project"/>
+            <TodoItem todo="Grocery shopping"/>
+            <TodoItem todo="Bake carrot cake"/>
+            <TodoItem todo="Exercise"/>
+        </div>
+    );
 }
 
 export default App;
